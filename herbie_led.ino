37be30b3a67d3648510afbda5e60860c26fb4a1b
srgelber <38055713+srgelber@users.noreply.github.com>
2022-04-02 18:58:09 -0700

    Serial bug fixed.

--- herbie_led.ino
+++ herbie_led.ino
@@ -16,13 +16,17 @@ byte input;
 bool command_ready = false;
 SoftwareSerial COM(9, 10);
 
+enum State{
+  START, KIT, CAUTION, OFF
+  };
+
 
 
 void setup() { 
   FastLED.addLeds<WS2812B,DATA_PIN,GRB>(leds,NUM_LEDS);
-  FastLED.setBrightness(84);
-  Serial.begin(115200);
-  COM.begin(115200);
+  FastLED.setBrightness(100);
+  Serial.begin(57600);
+  COM.begin(57600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Native USB only
   }
@@ -31,101 +35,173 @@ void setup() {
 
 
 void loop() {
+
+  static State state = START;
+  static int anim_speed = 1;
+  static int color = 0;
+
+  memset(&header, 0, sizeof(header));
     
-    
-  if(COM.available() > 0) {
-    header[0] = COM.read();
-    if(header[0] == 255){
-      uint8_t index = 1;
-      while(COM.available()){
-        header[index] = COM.read();
-        index++;
+  if(COM.available() > 3) {
+      //uint8_t index = 0;
+      //while(COM.available()){
+        //header[index] = COM.read();
+        //index++;
+      //}
+      for(int i = 0; i < 4; i++){
+        header[i] = COM.read();
       }
-      command_ready = true;
-
-    }
+      
+    
     //convert_header();
-   
-    //Serial.println(header[0], HEX);
-    //Serial.println(header[1], HEX);
-    //Serial.println(header[2], HEX);
-    //Serial.println(header[3], HEX);
-    //Serial.println(header[4], HEX);
+    Serial.print(header[0], HEX);
+    Serial.print(", ");
+    Serial.print(header[1], HEX);
+    Serial.print(", ");
+    Serial.print(header[2], HEX);
+    Serial.print(", ");
+    Serial.println(header[3], HEX);
+    
+    
+    
+    parseHeader(state, header, anim_speed, color);
 
     
-  
+    
   }
-  
-  if(header[2] == 195){
-    kit_anim();
-  }
-  else if(header[2] == 196){
-    warning_anim();
+
+  switch(state){
+    case KIT:
+      kit_anim(anim_speed, color);
+      break;
+    case CAUTION:
+      caution_anim(anim_speed, color);
+      break;
+    case START:
+      break;
+    case OFF:
+      off();
+      FastLED.show(); 
+      break;
+    default:
+      break;
+    
   }
   //Serial.println("done");
 }
 
-void convert_header(){
-  memset(&header, 0, sizeof(header));
-  static uint8_t index = 0;
-  
 
-   for (int i=0; i<4; i++) {
-      //while(!Serial.available()); // wait for a character
-    int incomingByte = Serial.read();
-    Serial.print(incomingByte,HEX);
-    Serial.print(' ');
+void parseHeader(State &state, byte header[], int &anim_speed, int &color){
+  if(header[0] == 0xFF){
+    if(header[1] == 0x01){
+      //Get Pattern
+      switch(header[2] >> 4){
+        case 0x01:
+          state = KIT;
+          break;
+        case 0x02:
+          state = CAUTION;
+          break;
+        case 0x00:
+          state = OFF;
+          break;
+        default:
+          state = START;
+          break;
+      }
+      
+      //Get Animation Speed(1-16)
+      if((header[2] & 0x0F) == 0){
+        anim_speed = 1;
+      }else{
+        anim_speed = (header[2] & 0x0F);
+      }
+      
+
+      //Get Color(0-255)
+      color = header[3];
+      
+    }else{
+      state = START;
     }
-    Serial.println();
-    
-  //while(Serial.available() > 0){
-    //recv_char = Serial.read();
-    //Serial.println("im here");
-    //header[index] = recv_char;
-    //index++;
-    //}
-  
+  }else{
+    state = START;
+  }
 }
-
 
 void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(110); } }
 
 void off() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(1); } }
 
-void kit_anim(){
+void kit_anim(int anim_speed, int color){
     static uint8_t hue = 0;
+    int temp_speed = 100 / anim_speed;
   // First slide the led in one direction
   for(int i = 0; i < NUM_LEDS; i++) {
     // Set the i'th led to red 
-    leds[i] = CHSV(0, 255, 255);
+    leds[i] = CHSV(color, 255, 255);
     // Show the leds
     FastLED.show(); 
     // now that we've shown the leds, reset the i'th led to black
     // leds[i] = CRGB::Black;
     fadeall();
     // Wait a little bit before we loop around and do it again
-    delay(30);
+    delay(temp_speed);
   }
 
   // Now go in the other direction.  
   for(int i = (NUM_LEDS)-1; i >= 0; i--) {
     // Set the i'th led to red 
-    leds[i] = CHSV(0, 255, 255);
+    leds[i] = CHSV(color, 255, 255);
     // Show the leds
     FastLED.show();
     // now that we've shown the leds, reset the i'th led to black
     // leds[i] = CRGB::Black;
     fadeall();
     // Wait a little bit before we loop around and do it again
-    delay(30);
+    delay(temp_speed);
   }
 }
 
-void warning_anim(){
+
+void rainbow_kit_anim(int anim_speed){
+    static uint8_t hue = 0;
+    int temp_speed = 100 / anim_speed;
+  // First slide the led in one direction
+  for(int i = 0; i < NUM_LEDS; i++) {
+    // Set the i'th led to red 
+    leds[i] = CHSV(hue, 255, 255);
+    // Show the leds
+    FastLED.show(); 
+    // now that we've shown the leds, reset the i'th led to black
+    // leds[i] = CRGB::Black;
+    fadeall();
+    // Wait a little bit before we loop around and do it again
+    delay(temp_speed);
+    hue++;
+  }
+
+  // Now go in the other direction.  
+  for(int i = (NUM_LEDS)-1; i >= 0; i--) {
+    // Set the i'th led to red 
+    leds[i] = CHSV(hue, 255, 255);
+    // Show the leds
+    FastLED.show();
+    // now that we've shown the leds, reset the i'th led to black
+    // leds[i] = CRGB::Black;
+    fadeall();
+    // Wait a little bit before we loop around and do it again
+    delay(temp_speed);
+    hue++;
+  }
+}
+
+void caution_anim(int anim_speed, int color){
+  int temp_speed = 400 / anim_speed;
   // First slide the led in one direction
   for(int i = 0; i < NUM_LEDS; i++) {
     if(i % 2 == 0){
-      leds[i] = CHSV(64, 255, 255);
+      leds[i] = CHSV(color, 255, 255);
     }
   }
     // Show the leds
@@ -134,13 +210,13 @@ void warning_anim(){
     // leds[i] = CRGB::Black;
     off();
     // Wait a little bit before we loop around and do it again
-    delay(200);
+    delay(temp_speed);
   
 
    // First slide the led in one direction
   for(int i = 0; i < NUM_LEDS; i++) {
     if(i % 2 != 0){
-      leds[i] = CHSV(64, 255, 255);
+      leds[i] = CHSV(color, 255, 255);
     }
   }
     // Show the leds
@@ -149,7 +225,7 @@ void warning_anim(){
     // leds[i] = CRGB::Black;
     off();
     // Wait a little bit before we loop around and do it again
-    delay(200);
+    delay(temp_speed);
   
   
 }
